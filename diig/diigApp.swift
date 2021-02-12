383784e1da8e334a2cf0cf9dbedd81d8171b2bbe
Radovan Paška <carnero@hey.com>
2021-02-12 17:31:57 +0100

    diig: create xcode project

---
+++ diig/diigApp.swift
@@ -0,0 +1,17 @@
+//
+//  diigApp.swift
+//  diig
+//
+//  Created by Radovan Paška on 12.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct diigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
